0c1209cc8cc3bd4ea3dfaff90e4e6bbf43fa10f0
kynestic <ducnguyenmanh.7791@gmail.com>
2024-10-21 01:56:18 +0700

    update

--- merge.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv(r'D:\CODING\Project\NVIDIA Stock prediction\data\embedding_text\BERT_EmbeddingText.csv')
-df2 = pd.read_csv(r'D:\CODING\Project\NVIDIA Stock prediction\data\raw\stock_news_price.csv')
-
-df1['timestamp'] = df2['timestamp']
-
-df1.to_csv(r'D:\CODING\Project\NVIDIA Stock prediction\data\embedding_text\BERT_EmbeddingText.csv', index=False)
